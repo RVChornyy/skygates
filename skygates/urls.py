cb452e1b9d45a6841b20c8bdebf6b7450353b184
Rost <rv4@ukr.net>
2023-11-23 16:44:52 +0200

    refactor skygates urls.py

--- skygates/urls.py
+++ skygates/urls.py
@@ -22,5 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("airport.urls", namespace="airport")),
+    path("accounts/", include("django.contrib.auth.urls"))
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
