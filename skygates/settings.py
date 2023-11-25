5372017b1029488f1cd2ff051d04dd24c55ae9e6
Rost <rv4@ukr.net>
2023-11-25 22:52:52 +0200

    add skygates.onrender.com to ALLOWED_HOSTS

--- skygates/settings.py
+++ skygates/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-#8!n%*&a78znnd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["127.0.0.1", "skygates.onrender.com"]
 
 
 # Application definition
